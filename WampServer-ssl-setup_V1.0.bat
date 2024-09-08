61fcca6424f848a3207c5602b96bf8e3b890363f
Manuel Silva <80219378+manuseiro@users.noreply.github.com>
2024-09-08 15:26:16 -0300

    Update WampServer-ssl-setup_V1.0.bat
    Explicação das Melhorias:
    - Estrutura organizada: A modularização do script em seções claras melhora a legibilidade e a manutenção.
    - Validação e segurança: Verifica se os diretórios e arquivos existem antes de continuar.
    - Mensagens de erro claras: Informa ao usuário o que deu errado e como corrigir.
    - Personalização do domínio: Apesar de neste exemplo continuar a usar localhost, você pode adaptar o script para permitir domínios personalizados.
    
    Mudanças Implementadas:
    - Após encontrar o diretório do WampServer, o script pede ao usuário para confirmar se o diretório está correto.
    - Caso o usuário responda que o diretório não está correto, ele poderá inserir manualmente o caminho desejado.
    - Solicitação de Informações do Certificado: O script agora solicita que o usuário insira as informações do certificado, como país, estado, localidade, organização, unidade organizacional e nome comum (CN). Valores padrão são fornecidos e usados se o usuário não inserir nenhum valor.
    - Senha da Chave Privada: O script permite que o usuário insira uma senha personalizada para a chave privada. Se o usuário não fornecer uma senha, a senha padrão "sua_senha" será utilizada.

--- WampServer-ssl-setup_V1.0.bat
+++ WampServer-ssl-setup_V1.0.bat
@@ -1,12 +1,13 @@
 @echo off
+
 :: Limpa a tela
 cls
 
 :: Habilita a expansão atrasada de variáveis
 setlocal enabledelayedexpansion 
 
-:: Define variáveis comuns de instalação
-set "diretorios_instalacao=C:\wamp64 C:\wamp D:\wamp64 D:\wamp"
+:: Define variáveis
+set "diretorios_instalacao=A:\wamp64 A:\wamp C:\wamp64 C:\wamp D:\wamp64 D:\wamp"
 set "diretorio_wamp="
 set "diretorio_apache="
 
@@ -14,7 +15,9 @@ set "diretorio_apache="
 chcp 65001 > nul
 
 :: Verifica se o script está sendo executado como administrador
-if not "%__APPDIR__%cacls.exe"=="%SYSTEMROOT%\system32\cacls.exe" (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system" && (
+    goto :executarScript
+) || (
     echo Por favor, execute este script como administrador.
     pause
     exit /b
@@ -22,117 +25,142 @@ if not "%__APPDIR__%cacls.exe"=="%SYSTEMROOT%\system32\cacls.exe" (
 
 :executarScript
 
-:: Verifica diretórios de instalação do WAMP
-call :verificar_diretorios
+:: Verificação da existência dos possíveis diretórios de instalação do WampServer
+for %%d in (%diretorios_instalacao%) do (
+    if exist "%%d" (
+        set "diretorio_wamp=%%d"
+        goto :confirmar_diretorio_wamp
+    )
+)
+
+:confirmar_diretorio_wamp
+if defined diretorio_wamp (
+    echo O diretório da instalação do WAMP encontrado é: !diretorio_wamp!
+    set /p "confirmacao=Está correto? (S/N): "
+    if /I "!confirmacao!" neq "S" (
+        set "diretorio_wamp="
+        goto :Definir_diretorio_wamp
+    )
+) else (
+    goto :Definir_diretorio_wamp
+)
+
+:Definir_diretorio_wamp
 if not defined diretorio_wamp (
     echo Não foi encontrado o diretório da instalação do WAMP.
     echo Exemplo: "C:\wamp64"
-    set /p "diretorio_wamp=Informe o diretório de instalação do WAMP: "
+    set /p "diretorio_wamp=Informe o diretório da instalação do WAMP: "
+
+    :: Verifica se o diretório informado não existe
     if not exist "!diretorio_wamp!" (
         echo O diretório "!diretorio_wamp!" não é válido.
         pause
         cls
-        goto :executarScript
+        goto :Definir_diretorio_wamp
     )
 )
-echo Diretório da instalação do WAMP encontrado: %diretorio_wamp%
+echo Diretório da instalação do WAMP confirmado: %diretorio_wamp%
 
 :: Busca o diretório de instalação do Apache
-call :verificar_apache
+for /d %%a in ("%diretorio_wamp%\bin\apache\apache*") do (
+    set "diretorio_apache=%%~fa"
+    goto :verificar_diretorio_apache
+)
+
+:verificar_diretorio_apache
 if not defined diretorio_apache (
+    cls
     echo Não foi encontrado o diretório da instalação do Apache.
     echo Verifique se a instalação WampServer está correta e tente novamente.
     exit /b
 ) else (
     echo Diretório da instalação do Apache encontrado: %diretorio_apache%
-    call :gerar_certificado
-    call :configurar_apache
-    echo Certificado instalado com sucesso.
+
+    cd /d "%diretorio_apache%"
+
+    set "OPENSSL_CONF=%diretorio_apache%\conf\openssl.cnf"
+
+    cd /d "%diretorio_apache%\bin"
+
+    :: Solicita as informações do certificado ao usuário
+    set /p "cert_country=Informe o código do país (C) [BR]: "
+    if "!cert_country!"=="" set "cert_country=BR"
+    
+    set /p "cert_state=Informe o estado (ST) [Ceara]: "
+    if "!cert_state!"=="" set "cert_state=Minas Gerais"
+    
+    set /p "cert_locality=Informe a localidade (L) [Fortaleza]: "
+    if "!cert_locality!"=="" set "cert_locality=Minas Gerais"
+    
+    set /p "cert_organization=Informe a organização (O) [localhost]: "
+    if "!cert_organization!"=="" set "cert_organization=localhost"
+    
+    set /p "cert_organizational_unit=Informe a unidade organizacional (OU) [localhost]: "
+    if "!cert_organizational_unit!"=="" set "cert_organizational_unit=localhost"
+    
+    set /p "cert_cn=Informe o nome comum (CN) [localhost]: "
+    if "!cert_cn!"=="" set "cert_cn=localhost"
+    
+    set /p "cert_password=Informe a senha da chave privada [sua_senha]: "
+    if "!cert_password!"=="" set "cert_password=sua_senha"
+
+    :: Cria o arquivo de configuração do certificado
+    echo authorityKeyIdentifier=keyid,issuer > domains.ext
+    echo basicConstraints=CA:FALSE >> domains.ext
+    echo keyUsage = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment >> domains.ext
+    echo subjectAltName = @alt_names >> domains.ext
+    echo [alt_names] >> domains.ext
+    echo DNS.1 = localhost >> domains.ext
+
+    :: Cria a requisição do certificado
+    openssl req -new -out server.csr -passout pass:%cert_password% -subj "/C=%cert_country%/ST=%cert_state%/L=%cert_locality%/O=%cert_organization%/OU=%cert_organizational_unit%/CN=%cert_cn%"
+
+    :: Gera a chave privada
+    openssl rsa -in privkey.pem -out server.key -passin pass:%cert_password%
+
+    :: Gera o certificado usando a requisição do certificado e a chave privada
+    openssl x509 -in server.csr -out server.crt -req -signkey server.key -days 3650 -sha256 -extfile domains.ext
+    
+    :: Deleta arquivos desnecessários
+    del privkey.pem
+    del server.csr
+    del domains.ext
+
+    :: Move os arquivos para o diretório de configuração do Apache
+    move /y server.crt %diretorio_apache%\conf\server.crt
+    move /y server.key %diretorio_apache%\conf\server.key    
 )
 
-:: Abre a página do projeto no navegador
-start https://github.com/abmvdigital/wampserver-ssl-setup
-
-exit /b
-
-:verificar_diretorios
-for %%d in (%diretorios_instalacao%) do (
-    if exist "%%d" (
-        set "diretorio_wamp=%%d"
-        exit /b
-    )
-)
-
-:: Se não encontrou, procura no registro do Windows
-for /f "tokens=3*" %%a in ('reg query "HKLM\Software\WAMPServer" /v InstallDir 2^>nul') do (
-    if exist "%%b" (
-        set "diretorio_wamp=%%b"
-        exit /b
-    )
-)
-exit /b
-
-:verificar_apache
-for /d %%a in ("%diretorio_wamp%\bin\apache\apache*") do (
-    set "diretorio_apache=%%~fa"
-    exit /b
-)
-exit /b
-
-:gerar_certificado
-cd /d "%diretorio_apache%\bin"
-
-:: Solicita ao usuário a senha (opcional)
-set /p "senha_certificado=Digite a senha para o certificado (deixe em branco para padrão): "
-if "%senha_certificado%"=="" set "senha_certificado=sua_senha"
-
-:: Solicita ao usuário informações do certificado (opcional)
-set "default_subj=/C=BR/ST=Minas Gerais/L=Minas Gerais/O=localhost/OU=localhost/CN=localhost"
-set /p "informacoes_certificado=Digite as informações do certificado (deixe em branco para padrão): "
-if "%informacoes_certificado%"=="" set "informacoes_certificado=%default_subj%"
-
-:: Cria o arquivo de configuração do certificado
-(
-echo authorityKeyIdentifier=keyid,issuer
-echo basicConstraints=CA:FALSE
-echo keyUsage = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment
-echo subjectAltName = @alt_names
-echo [alt_names]
-echo DNS.1 = localhost
-) > domains.ext
-
-:: Cria a requisição do certificado
-openssl req -new -out server.csr -passout pass:%senha_certificado% -subj "%informacoes_certificado%"
-
-:: Gera a chave privada
-openssl rsa -in privkey.pem -out server.key -passin pass:%senha_certificado%
-
-:: Gera o certificado
-openssl x509 -in server.csr -out server.crt -req -signkey server.key -days 3650 -sha256 -extfile domains.ext
-
-:: Remove arquivos desnecessários
-del privkey.pem server.csr domains.ext
-
-:: Move os arquivos para o diretório de configuração do Apache
-move /y server.crt "%diretorio_apache%\conf\server.crt"
-move /y server.key "%diretorio_apache%\conf\server.key"
-
-exit /b
-
-:configurar_apache
-:: Configura o arquivo httpd.conf
+:: Bloco de código para configurar o arquivo httpd.conf
 set "arquivo_httpd_conf=%diretorio_apache%\conf\httpd.conf"
+
+:: Substitui "#LoadModule ssl_module modules/mod_ssl.so" por "LoadModule ssl_module modules/mod_ssl.so"
 powershell -Command "(Get-Content '%arquivo_httpd_conf%') -replace '#LoadModule ssl_module modules/mod_ssl.so', 'LoadModule ssl_module modules/mod_ssl.so' | Set-Content '%arquivo_httpd_conf%'"
+
+:: Substitui "#Include conf/extra/httpd-ssl.conf" por "Include conf/extra/httpd-ssl.conf"
 powershell -Command "(Get-Content '%arquivo_httpd_conf%') -replace '#Include conf/extra/httpd-ssl.conf', 'Include conf/extra/httpd-ssl.conf' | Set-Content '%arquivo_httpd_conf%'"
+
+:: Substitui "#LoadModule socache_shmcb_module modules/mod_socache_shmcb.so" por "LoadModule socache_shmcb_module modules/mod_socache_shmcb.so"
 powershell -Command "(Get-Content '%arquivo_httpd_conf%') -replace '#LoadModule socache_shmcb_module modules/mod_socache_shmcb.so', 'LoadModule socache_shmcb_module modules/mod_socache_shmcb.so' | Set-Content '%arquivo_httpd_conf%'"
 
-:: Configura o arquivo httpd-ssl.conf
+:: Bloco de código para configurar o arquivo httpd-ssl.conf
 set "arquivo_httpd_ssl_conf=%diretorio_apache%\conf\extra\httpd-ssl.conf"
-powershell -Command "(Get-Content '%arquivo_httpd_ssl_conf%') -replace 'DocumentRoot.*', 'DocumentRoot \"${INSTALL_DIR}/www\"' | Set-Content '%arquivo_httpd_ssl_conf%'"
+
+:: Substitui a linha "DocumentRoot" por "DocumentRoot "${INSTALL_DIR}/www""
+powershell -Command "(Get-Content '%arquivo_httpd_ssl_conf%') -replace 'DocumentRoot.*', 'DocumentRoot "${INSTALL_DIR}/www"' | Set-Content '%arquivo_httpd_ssl_conf%'"
+
+:: Substitui a linha "ServerName" por "ServerName localhost:443"
 powershell -Command "(Get-Content '%arquivo_httpd_ssl_conf%') -replace 'ServerName.*', 'ServerName localhost:443' | Set-Content '%arquivo_httpd_ssl_conf%'"
 
 :: Instala o certificado
 set "caminho_certificado=%diretorio_apache%\conf\server.crt"
 certutil -addstore -user "Root" "%caminho_certificado%"
 
-exit /b
+cls
+
+echo Certificado instalado com sucesso.
+
+pause
+
+:: Abre a página do projeto no navegador
+start https://github.com/manuseiro/wampserver-ssl-setup
